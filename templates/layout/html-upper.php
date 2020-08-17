32396d1f3fc2ef800e8de2ce0ce345e2ab2511b3
julius <phpninja@mail.com>
2020-08-17 14:10:54 -0700

    made a few adjustments to the markup

--- templates/layout/html-upper.php
+++ templates/layout/html-upper.php
@@ -7,15 +7,15 @@
  */
 ?>
 
-
 <html lang="en" data-ng-app="cbConnectApp">
 
 <head>
     <meta charset="UTF-8">
-    <title>Hack Match, find a mentor, mentee, or code collaborator</title>
+    <title>Hack Match: find a mentor, mentee, or collaborator</title>
     
-    <!-- Angular Material CSS now available via Google CDN; version 1.1.9 used here -->
-    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/angular_material/1.1.9/angular-material.min.css">
+    <!-- Angular Material CSS now available via Google CDN; version 1.1.9 used here
+    "https://ajax.googleapis.com/ajax/libs/angular_material/1.1.9/angular-material.min.css"-->
+    <link rel="stylesheet" href="/ui-match/modules/ngmd-min.css">
     <link rel="stylesheet" href="/ui-match/styles/app.css">
     
     <!-- ng-cloak -->
