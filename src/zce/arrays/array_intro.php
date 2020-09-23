480bdac32711e57aa0ee7e9c1a3bbcaf8a838f36
julius <phpninja@mail.com>
2020-09-23 00:16:44 -0700

    Create array_intro.php

---
+++ src/zce/arrays/array_intro.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 9/13/2020
+ * Time: 7:54 AM
+ *
+ * "Because thinking of var names is hard"
+ * -- short var names --
+ * set, rec, dim, per, vec, cep, tro, foo, net, fu, bar, cas, xi, nu, mu, rho, fi, a, b, c, ds, x, y, z, stat
+ * -- cool var names --
+ * perceptron, metric, nueronet, cascade, cluster, matrix, feature, omicron, sigma, iota, epsilon, gamma, omega,
+ * -- combinations --
+ * metricSet, cascadeVec, iotaStat, epsilonFoo, featureX, nueronetA, etc.
+ */
+return <<<ANGULAR_JS_ENGINEER_SKILL_CONTENT
+<h4 class="ja-src-code-link">
+    Source Code for this cloud app page:
+    <a href="https://github.com/Hack-Match/hackmatch/zce/arrays">HackMatch/arrays</a>
+</h4>
+
+<div class="ja-content">
+    <h3>With a lot of Power, comes a lot of Memory</h3>
+    <p>
+        PHPs' arrays get used for everything. They're super easy to learn and use <br>
+        ... but they can COST Megabytes OR Gigabytes of memory. This is when I pass that data to  <br>
+        a db server & invoke a SQL <b>stored procedure</b> to operate in sync w/PHP.
+    </p>
+    <p>
+        However, for us <i>Practical</i> Computer Science type, we can use alternate data structures <br>
+        or clever hacks to improve the array. PHP after all, is VERY powerful, I like to think of it <br>
+        as the skin over the C source code muscle.
+    </p>
+    <p>
+        To improve the runtime memory efficiency, we can use Streams, SPL Data Structures, <br>
+        C extensions, & generators to yield a data table and just use an array per row.
+    </p>
+</div>
+ANGULAR_JS_ENGINEER_SKILL_CONTENT;
