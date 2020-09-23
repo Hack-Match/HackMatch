4098670a7b1cf6a2eeba7c1bf44427a285a32a2c
julius <phpninja@mail.com>
2020-09-23 00:16:21 -0700

    Create OOPTopicStruct.php

---
+++ src/structs/OOPTopicStruct.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 9/15/2020
+ * Time: 2:45 AM
+ */
+
+
+namespace CodeBuddies;
+
+
+class OOPTopicStruct
+{
+    // Landing page vars
+    public string $icon = 'class';
+    public string $title = 'Object Oriented Programming';
+    public string $href = '/julius/oop';
+    
+    // AngularJS content vars
+    public string $intro = 'oop_intro';
+    public string $adv_oop = 'oop_advanced_oop';
+    public string $anon = 'oop_anonymous_objects';
+    public string $auto = 'oop_auto_loading';
+    public string $bas_oop = 'oop_basic_oop';
+    public string $gen = 'oop_generators';
+    public string $in = 'oop_inheritance';
+    public string $late = 'oop_late_static_binding';
+    public string $loop = 'oop_loop_compare';
+    public string $magic = 'oop_magic_methods';
+    public string $reflect = 'oop_reflection';
+    public string $spl = 'oop_spl';
+    public string $trait = 'oop_traits';
+    public string $try = 'oop_try_catch';
+}
