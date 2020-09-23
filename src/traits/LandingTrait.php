21629fd808db15ad40cf32268606d89f7c148def
julius <phpninja@mail.com>
2020-09-23 00:16:36 -0700

    Create LandingTrait.php

---
+++ src/traits/LandingTrait.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 9/17/2020
+ * Time: 9:48 PM
+ *
+ * "Because thinking of var names is hard"
+ * -- short var names --
+ * set, rec, dim, per, vec, cep, tro, foo, net, fu, bar, cas, xi, nu, mu, rho, fi, a, b, c, ds, x, y, z, stat
+ * -- cool var names --
+ * perceptron, metric, nueronet, cascade, cluster, matrix, feature, omicron, sigma, iota, epsilon, gamma, omega,
+ * -- combinations --
+ * metricSet, cascadeVec, iotaStat, epsilonFoo, featureX, nueronetA, etc.
+ */
+ 
+namespace CodeBuddies;
+
+trait LandingTrait
+{
+    // Landing page vars
+    public string $title;
+    public string $icon;
+    public string $href;
+}
