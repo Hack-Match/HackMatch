5bbfbe311b2a9a7f0a48289bd163c5fdad381273
julius <phpninja@mail.com>
2020-09-23 00:18:27 -0700

    Create errors_intro.php

---
+++ src/zce/errors/errors_intro.php
@@ -0,0 +1,41 @@
+<?php
+/*
+     "Because thinking of var names is hard"
+     -- short var names --
+     set, rec, dim, per, vec, cep, tro, foo, net, fu, bar, cas, xi, nu, mu, rho, fi, a, b, c, ds, x, y, z, stat
+     -- cool var names --
+     perceptron, metric, nueronet, cascade, cluster, matrix, feature, omicron, sigma, iota, epsilon, gamma, omega,
+     -- combinations --
+     metricSet, cascadeVec, iotaStat, epsilonFoo, featureX, nueronetA, etc.
+
+//-- functions/ops:
+- "+" op
+
+
+*/
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 9/14/2020
+ * Time: 7:35 PM
+ */
+
+
+return <<<ANGULAR_JS_ENGINEER_SKILL_CONTENT
+<h4 class="ja-src-code-link">
+    Source Code for this app page:
+    <a href="https://github.com/Hack-Match/hackmatch/zce/errors">HackMatch/errors</a>
+</h4>
+<div class="ja-content">
+    <h3>Squashing Bugs before they become 🐜's </h3>
+    <p>
+        I'll admit, on occasion I get in "JUST make it work" mode
+        and start to use <code>\Throwable</code> for everything
+    </p>
+    
+    <p>
+        However, a much better approach is using the Rich selection of built-in
+        specific exceptions PHP offers.
+    </p>
+</div>
+ANGULAR_JS_ENGINEER_SKILL_CONTENT;
