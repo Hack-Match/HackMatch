7f6114c2e97f4f0362ec4c1bd2444534da813d14
julius <phpninja@mail.com>
2020-09-23 00:21:57 -0700

    Create output_buffer.php

---
+++ src/zce/web/output_buffer.php
@@ -0,0 +1,60 @@
+<?php
+/*
+     "Because thinking of var names is hard"
+     -- short var names --
+     set, rec, dim, per, vec, cep, tro, foo, net, fu, bar, cas, xi, nu, mu, rho, fi, a, b, c, ds, x, y, z, stat
+     -- cool var names --
+     perceptron, metric, nueronet, cascade, cluster, matrix, feature, omicron, sigma, iota, epsilon, gamma, omega,
+     -- combinations --
+     metricSet, cascadeVec, iotaStat, epsilonFoo, featureX, nueronetA, etc.
+
+//-- functions/ops:
+- "+" op
+
+
+*/
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 9/14/2020
+ * Time: 7:35 PM
+ */
+
+function ja_output_buffer() {
+
+}
+
+$output_buffer_code = <<<'output_buffer'
+
+output_buffer;
+
+$output_buffer_output = ja_output_buffer();
+
+
+
+
+return <<<ANGULAR_JS_ENGINEER_SKILL_CONTENT
+<div id="fs_info_op_code" class="ja-content" layout-padding layout-margin>
+    <h2>... I'm working on this example.<br>I'll have examples of all skills between:<br>9-27-2020 to 10-10-2020</h2>
+    <p class="ja-personal-experience">
+        When building automated data processing systems and data applications <br>
+        with an HTML5 based User Interface... <br>
+    </p>
+    <h5 class="ja-techniques-used">
+        In the example, the functions/classes/techniques used are: <br>
+        some_function(), SomeClass, etc.
+    </h5>
+    
+    <!-- Sample code -->
+    <pre>$ sample_code</pre>
+</div>
+
+<br><br>
+
+<div id="fs_resource_output" class="ja-content" flex="60" layout-padding layout-margin>
+    <h3>The output from the above code:</h3>
+
+    <!-- Code output -->
+    <div>$ code_output</div>
+</div>
+ANGULAR_JS_ENGINEER_SKILL_CONTENT;
