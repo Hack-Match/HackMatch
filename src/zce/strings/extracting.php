9074e2f3c1671e021bfc9cfbd06724770fba5873
julius <phpninja@mail.com>
2020-09-23 00:21:13 -0700

    Create extracting.php

---
+++ src/zce/strings/extracting.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 9/14/2020
+ * Time: 7:53 PM
+ *
+ * "Because thinking of var names is hard"
+ * -- short var names --
+ * set, rec, dim, per, vec, cep, tro, foo, net, fu, bar, cas, xi, nu, mu, rho, fi, a, b, c, ds, x, y, z, stat
+ * -- cool var names --
+ * perceptron, metric, nueronet, cascade, cluster, matrix, feature, omicron, sigma, iota, epsilon, gamma, omega,
+ * -- combinations --
+ * metricSet, cascadeVec, iotaStat, epsilonFoo, featureX, nueronetA, etc.
+ */
+ 
+
+
