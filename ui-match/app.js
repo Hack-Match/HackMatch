3a33cde189b08c89261a8602c36f982309f36636
julius <phpninja@mail.com>
2020-09-23 00:23:37 -0700

    Update app.js

--- ui-match/app.js
+++ ui-match/app.js
@@ -2,30 +2,47 @@
  * Created by Julius Alvarado on 5/23/2020.
  */
 
-"use strict";
+(function(){
+    "use strict";
 
-// , 'material.svgAssetsCache'
-angular.module('cbConnectApp', ['ngMaterial', 'ngMessages', 'ngMdIcons']);
+    // , 'material.svgAssetsCache'
+    angular.module('hackMatchCloudApp', ['ngMaterial', 'ngMessages', 'ngMdIcons', 'ngSanitize']);
 
-angular.module('cbConnectApp').controller('MatchCtrl', ['$scope', MatchClass]);
+    let app = angular.module('hackMatchCloudApp');
+    app.controller('MatchCtrl', ['$scope', '$mdDialog', MatchClass]);
 
-function MatchClass($scope) {
-    $scope.status = 'Wired up to AngularJS 1.6';
-    $scope.data = {};
+    function MatchClass($scope, $mdDialog) {
+        $scope.status = 'Wired up to AngularJS 1.7';
+        $scope.data = {};
 
-    // looking for view
-    $scope.data.account = true;
-    $scope.data.coding = false;
-    $scope.data.mentor = false;
-    $scope.data.mentee = false;
-    $scope.data.openSource = false;
-    $scope.data.contributors = false;
-    $scope.other = false;
+        // looking for view
+        $scope.data.account = true;
+        $scope.data.coding = false;
+        $scope.data.mentor = false;
+        $scope.data.mentee = false;
+        $scope.data.openSource = false;
+        $scope.data.contributors = false;
+        $scope.other = false;
 
-    $scope.s_imgPath = '/img/hack-match300.png'
-    let r = Math.floor(Math.random() * (16-1)) + 1;
-    $scope.s_avatarImg = `/img/avatars/a${r}.png`;
+        $scope.s_imgPath = '/img/hack-match300.png'
+        let r = Math.floor(Math.random() * (16 - 1)) + 1;
+        $scope.s_avatarImg = `/img/avatars/a${r}.png`;
 
-    // contact info view
-    $scope.showHints = true;
-}
+        // contact info view
+        $scope.showHints = true;
+
+        $scope.scMatchCtrl_showModal = function(ev) {
+            $mdDialog.show({
+                contentElement: '#ja-side-node',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true
+            });
+        };
+
+        $scope.scMatchCtrl_listItem = function(ev) {
+          console.log('Just a neat effect.');
+        };
+    }
+
+})();
