8ee55b544f6812ae66750cce5c47a94ac38f9165
julius <phpninja@mail.com>
2020-09-23 00:22:58 -0700

    Create html-lower.php

---
+++ templates/layout/html-lower.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 5/25/2020
+ * Time: 10:33 PM
+ */
+?>
+
+<!--------------------------------------------------
+------------ Content Delivery Networks ------------
+----------------------------------------------------
+
+- 1 -
+"https://ajax.googleapis.com/ajax/libs/angularjs/1.7.2/angular.min.js"
+"/ui-match/modules/ng.js"
+
+- 2 -
+"https://ajax.googleapis.com/ajax/libs/angularjs/1.7.2/angular-animate.min.js"
+"/ui-match/modules/ng-animate.js"
+
+- 3 -
+"https://ajax.googleapis.com/ajax/libs/angularjs/1.7.2/angular-aria.min.js"
+"/ui-match/modules/ng-aria.js"
+
+- 4 -
+"https://ajax.googleapis.com/ajax/libs/angularjs/1.7.2/angular-messages.min.js"
+"/ui-match/modules/ng-messages.js"
+
+- 5 -
+"//cdnjs.cloudflare.com/ajax/libs/angular-material-icons/0.7.1/angular-material-icons.min.js"
+"/ui-match/modules/ngmd-icons.js"
+
+- 6 -
+"https://ajax.googleapis.com/ajax/libs/angular_material/1.1.9/angular-material.min.js"
+"/ui-match/modules/ngmd.js"
+-->
+
+<!-- AngularJS Core -->
+<script src="/ui-match/modules/ng.js"></script>
+<script src="/ui-match/modules/ng-animate.js"></script>
+<script src="/ui-match/modules/ng-aria.js"></script>
+<script src="/ui-match/modules/ng-messages.js"></script>
+<script src="/ui-match/modules/ng-sanitize.js"></script>
+
+<!-- AngularJS Material v1.1.9 -->
+<script src="/ui-match/modules/ngmd-icons.js"></script>
+<script src="/ui-match/modules/ngmd.js"></script>
+
+<!-- HackMatch.io AngularJS 1.7 app -->
+<script src="/ui-match/app.js"></script>
