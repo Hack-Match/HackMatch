88fc8a292985be7c6b9d3bf19e49993e47e1cf18
julius <phpninja@mail.com>
2020-09-23 00:19:26 -0700

    Create oop_intro.php

---
+++ src/zce/oop/oop_intro.php
@@ -0,0 +1,25 @@
+<?php
+/*
+     "Because thinking of var names is hard"
+     -- short var names --
+     set, rec, dim, per, vec, cep, tro, foo, net, fu, bar, cas, xi, nu, mu, rho, fi, a, b, c, ds, x, y, z, stat
+     -- cool var names --
+     perceptron, metric, nueronet, cascade, cluster, matrix, feature, omicron, sigma, iota, epsilon, gamma, omega,
+     -- combinations --
+     metricSet, cascadeVec, iotaStat, epsilonFoo, featureX, nueronetA, etc.
+*/
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 9/14/2020
+ * Time: 7:35 PM
+ */
+return <<<OOP_INTRO_ANGULAR_JS_CONTENT
+<section id="intro-oop" layout="column" layout-align="center center" layout-padding layout-margin>
+    <h1>Object Oriented Programming</h1>
+    <p>
+        Source Code for this app page:
+        <a href="https://github.com/Hack-Match/hackmatch/zce/io/io.php">HackMatch/io.php</a>
+    </p>
+</section>
+OOP_INTRO_ANGULAR_JS_CONTENT;
