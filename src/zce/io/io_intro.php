3b23d3531176631fc0bf8f0b1adb649ef0474269
julius <phpninja@mail.com>
2020-09-23 00:18:59 -0700

    Create io_intro.php

---
+++ src/zce/io/io_intro.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 9/13/2020
+ * Time: 1:45 AM
+ *
+ * "Because thinking of var names is hard"
+ * -- short var names --
+ * set, rec, dim, per, vec, cep, tro, foo, net, fu, bar, cas, xi, nu, mu, rho, fi, a, b, c, ds, x, y, z, stat
+ * -- cool var names --
+ * perceptron, metric, nueronet, cascade, cluster, matrix, feature, omicron, sigma, iota, epsilon, gamma, omega,
+ * -- combinations --
+ * metricSet, cascadeVec, iotaStat, epsilonFoo, featureX, nueronetA, etc.
+ */
+
+return <<<'IO_INTRO'
+        <h4 class="ja-src-code-link">
+            Source Code for this app page:
+            <a href="https://github.com/Hack-Match/hackmatch/zce/io/io.php">HackMatch/io.php</a>
+        </h4>
+
+        <div class="ja-content">
+            <h3>Here is sample data from the primary resource</h3>
+            <pre>
+job_id,job_name,qty,job_type,export_duedate,export_coordinator,invoice,postage_class,postage_amt,export_vend_name,sales_rep
+64755,RTECH TIPS CO 11/25/19,39149,Self Mailer,11/26/2019 0:00,Heather Lunsford,603,Standard,6509.26,"[""Denver Redstone Production""]",Contact user not set
+65705,Got Junk Nov,7136,Post Card,11/1/2019 0:00,Leslie Moores,245920,Standard,1,"[""RS W.Sac""]",Andy Cody
+            </pre>
+        </div>
+IO_INTRO;
