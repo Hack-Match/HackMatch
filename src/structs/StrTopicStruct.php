8cc6bcf7cee401c7edaa6f82f9f1c1a764edcaf1
julius <phpninja@mail.com>
2020-09-23 00:16:28 -0700

    Create StrTopicStruct.php

---
+++ src/structs/StrTopicStruct.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 9/15/2020
+ * Time: 2:17 AM
+ */
+
+
+namespace CodeBuddies;
+
+
+class StrTopicStruct
+{
+    // Landing page vars
+    public string $title = 'Strings & The Regex Engine';
+    public string $icon = 'translate';
+    public string $href = '/julius/strings';
+    
+    // AngularJS conent vars
+    public string $enc = 'str_encodes';
+    public string $ext = 'str_extracting';
+    public string $form = 'str_formatting';
+    public string $here = 'str_heredoc_nowdoc';
+    public string $pho = 'str_phonetic';
+    public string $reg_adv = 'str_regex_engine_advanced';
+    public string $reg_bas = 'str_regex_engine_basics';
+    public string $reg_int = 'str_regex_engine_intermediate';
+    public string $search = 'str_search_compare';
+    public string $intro = 'str_string_intro';
+    public string $tr = 'str_transforms';
+    public string $word = 'str_word_char_count';
+}
