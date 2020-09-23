d081684b32b3d6cdc7c26803856865c5e9cb5182
julius <phpninja@mail.com>
2020-09-23 00:17:06 -0700

    Create basics_intro.php

---
+++ src/zce/basics/basics_intro.php
@@ -0,0 +1,51 @@
+<?php
+/*
+     "Because thinking of var names is hard"
+     -- short var names --
+     set, rec, dim, per, vec, cep, tro, foo, net, fu, bar, cas, xi, nu, mu, rho, fi, a, b, c, ds, x, y, z, stat
+     -- cool var names --
+     perceptron, metric, nueronet, cascade, cluster, matrix, feature, omicron, sigma, iota, epsilon, gamma, omega,
+     -- combinations --
+     metricSet, cascadeVec, iotaStat, epsilonFoo, featureX, nueronetA, etc.
+
+//-- functions/ops:
+- "+" op
+
+
+*/
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 9/13/2020
+ * Time: 7:54 AM
+ */
+return <<<BASICS_INTRO_CONTENT
+<div id="fs_info_op_code" class="ja-content" layout-padding layout-margin>
+    <h2>
+        The basic building blocks.
+    </h2>
+    <p class="ja-personal-experience">
+        I've been programming since I was 18 years old. So I've learned this part over and OVER <br>
+        again in the excessive amount of languages there are.<br>
+        After 12+ years I'm digging my heals in at PHP, unless I get paid to learn a different language.
+    </p>
+    
+    <p class="ja-personal-experience">
+        All the main languages are a descendent of the <b>C programming language</b> <br>
+        so they're each pretty similar. <br>
+    </p>
+    
+    <p class="ja-personal-experience">
+        The breakdown of my <i>Programming Language</i> experience: <br>
+        <ul>
+        <li>18-22 years old: Visual Basic, C, Python, C++, & a bit of Java (but I was a C++ Evangelist back then)</li>
+        <li>23 years old: Focused more on General Education classes & my "Retail Auditor" job.</li>
+        <li>24-25 years old: C# via the Unity Game Engine. <b>Dropped out of College Officially :'(</b></li>
+        <li>26-27 years old: AngularJS, JavaScript, Node.js</li>
+        <li>28-Present: PHP, SQL, Node.js, JavaScript, AngularJS and a bit of .NET C#</li>
+        </ul>
+    </p>
+</div>
+
+<br><br>
+BASICS_INTRO_CONTENT;
