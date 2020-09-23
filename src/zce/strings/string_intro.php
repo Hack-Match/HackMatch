948ea3179da1b0c3d6011c80f405a6cad7cfb5b2
julius <phpninja@mail.com>
2020-09-23 00:21:35 -0700

    Create string_intro.php

---
+++ src/zce/strings/string_intro.php
@@ -0,0 +1,40 @@
+<?php
+/*
+     "Because thinking of var names is hard"
+     -- short var names --
+     set, rec, dim, per, vec, cep, tro, foo, net, fu, bar, cas, xi, nu, mu, rho, fi, a, b, c, ds, x, y, z, stat
+     -- cool var names --
+     perceptron, metric, nueronet, cascade, cluster, matrix, feature, omicron, sigma, iota, epsilon, gamma, omega,
+     -- combinations --
+     metricSet, cascadeVec, iotaStat, epsilonFoo, featureX, nueronetA, etc.
+
+//-- functions/ops:
+- "+" op
+
+
+*/
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 9/14/2020
+ * Time: 7:35 PM
+ */
+ 
+return <<<STRING_INTRO
+<div layout="column" layout-align="center center">
+    <h2>Strings & The Regex Engine</h2>
+    <p class="ja-src-code-link">
+        Source Code for this app page:
+        <a href="https://github.com/Hack-Match/hackmatch/src/zce/strings">HackMatch/strings.php</a>
+    </p>
+
+    <div class="ja-content" layout-padding>
+        <p class="ja-intro-topic">
+            I don't know what I'd do without regex, let's take a look at matching
+            Custom Inventory Usage the inventory team types in the "notes" field:
+        </p>
+    </div>
+</div>
+STRING_INTRO;
+
+
