0e96526e92499e76d0e8a2b8bfd0b4d6fa8563fd
julius <phpninja@mail.com>
2020-09-23 00:16:11 -0700

    Create BasicTopicStruct.php

---
+++ src/structs/BasicTopicStruct.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 9/15/2020
+ * Time: 2:58 AM
+ */
+
+
+namespace CodeBuddies;
+
+
+class BasicTopicStruct
+{
+    // Landing page vars
+    public string $title = 'Programming Fundamentals';
+    public string $icon = 'developer_mode';
+    public string $href = '/julius/basics';
+    
+    // AngularJS content vars
+    public string $intro = 'bas_basics_intro';
+    public string $config = 'bas_configuration';
+    public string $control = 'bas_control_structures';
+    public string $ext = 'bas_extensions';
+    public string $id = 'bas_identifiers';
+    public string $lang = 'bas_language_constructs';
+    public string $name = 'bas_namespaces';
+    public string $op = 'bas_operators';
+    public string $per = 'bas_performance';
+}
