08632499529f01caef08d309cdc60815d7b8f85f
julius <phpninja@mail.com>
2020-09-23 00:16:09 -0700

    Create ArrTopicStruct.php

---
+++ src/structs/ArrTopicStruct.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 9/15/2020
+ * Time: 2:20 AM
+ */
+
+
+namespace CodeBuddies;
+
+
+class ArrTopicStruct
+{
+    // The landing page vars
+    public string $title = 'Arrays & Data Structures';
+    public string $icon = 'view_column';
+    public string $href = '/julius/arrays';
+    
+    // The AngularJS content vars
+    public string $intro = 'arr_array_intro';
+    public string $ops = 'arr_array_ops';
+    public string $key = 'arr_key_values';
+    public string $loop = 'arr_looping';
+    public string $merge = 'arr_merge_compare';
+    public string $sort = 'arr_sorting';
+    public string $spl = 'arr_spl';
+    public string $stack = 'arr_stack_queue';
+    public string $tr = 'arr_transforms';
+}
