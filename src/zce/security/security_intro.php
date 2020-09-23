454c27b29b04e6e7c821a9fb3ecea0d1ec5881e2
julius <phpninja@mail.com>
2020-09-23 00:19:53 -0700

    Create security_intro.php

---
+++ src/zce/security/security_intro.php
@@ -0,0 +1,43 @@
+<?php
+/*
+     "Because thinking of var names is hard"
+     -- short var names --
+     set, rec, dim, per, vec, cep, tro, foo, net, fu, bar, cas, xi, nu, mu, rho, fi, a, b, c, ds, x, y, z, stat
+     -- cool var names --
+     perceptron, metric, nueronet, cascade, cluster, matrix, feature, omicron, sigma, iota, epsilon, gamma, omega,
+     -- combinations --
+     metricSet, cascadeVec, iotaStat, epsilonFoo, featureX, nueronetA, etc.
+
+//-- functions/ops:
+- "+" op
+
+
+*/
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 9/14/2020
+ * Time: 7:35 PM
+ */
+
+
+
+return <<<SECURITY_INTRO_CONTENT
+<h4 class="ja-src-code-link">
+    Source Code for this app page:
+    <a href="https://github.com/Hack-Match/hackmatch/zce/security">HackMatch/security</a>
+</h4>
+<div class="ja-content" layout-padding layout-margin>
+    <p class="ja-personal-experience">
+        Working as an on-premise Data Engineer these past couple of years,
+        I had the luxury of working within firewalls upon firewalls right in
+        the center of our intrAnet. Our SQL Servers room was locked behind a
+        reinforced quadra locked door with 24/7 camera's.
+    </p>
+    <p class="ja-personal-experience">
+        Thanks to the Corona virus that business had to layoff tons of workers, so
+        I'm back in the real world. And PHP is still perceived as a web language
+        (even though I was using it for Data Analysis & Data Engineering).
+    </p>
+</div>
+SECURITY_INTRO_CONTENT;
