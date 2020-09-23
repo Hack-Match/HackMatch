63b9cee055b78fca59480c6b2be1b111bd38930d
julius <phpninja@mail.com>
2020-09-23 00:18:37 -0700

    Create functions_intro.php

---
+++ src/zce/functions/functions_intro.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 9/13/2020
+ * Time: 7:55 AM
+ *
+ * "Because thinking of var names is hard"
+ * -- short var names --
+ * set, rec, dim, per, vec, cep, tro, foo, net, fu, bar, cas, xi, nu, mu, rho, fi, a, b, c, ds, x, y, z, stat
+ * -- cool var names --
+ * perceptron, metric, nueronet, cascade, cluster, matrix, feature, omicron, sigma, iota, epsilon, gamma, omega,
+ * -- combinations --
+ * metricSet, cascadeVec, iotaStat, epsilonFoo, featureX, nueronetA, etc.
+ */
+ 
+
+
+return <<<'FUNCTIONS_INTRO'
+<h4 class="ja-src-code-link">
+    Source Code for this app page:
+    <a href="https://github.com/Hack-Match/hackmatch/zce/functions">HackMatch/functions</a>
+</h4>
+
+<div class="ja-content">
+    <h3>Why I find functional programming to be useful:</h3>
+    <p>
+        Although PHP isn't as much of a functional programming language as JavaScript, <br>
+        all of the practical features can still be emulated. In PHP we can use the "use" <br>
+        keyword to get access to outer scope which is <b>immensely</b> useful.
+    </p>
+</div>
+FUNCTIONS_INTRO;
