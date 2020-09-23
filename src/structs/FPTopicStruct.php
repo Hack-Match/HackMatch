344b94c24b95bd38d9d1740a1ab72749dd594a3f
julius <phpninja@mail.com>
2020-09-23 00:16:18 -0700

    Create FPTopicStruct.php

---
+++ src/structs/FPTopicStruct.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 9/15/2020
+ * Time: 2:47 AM
+ */
+
+namespace CodeBuddies;
+
+class FPTopicStruct
+{
+    // Landing page vars
+    public string $title = 'Functional Programming';
+    public string $icon = 'functions';
+    public string $href = '/julius/functions';
+    
+    // AngularJS content vars
+    public string $intro = 'fp_intro';
+    public string $param = 'fp_parametric_rules';
+    public string $lambda = 'fp_lambda_closure';
+    public string $var = 'fp_variadic_dynamic';
+    public string $call = 'fp_callable_class';
+    public string $ref = 'fp_references';
+    public string $scope = 'fp_scope';
+}
